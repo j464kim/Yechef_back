be35ff57eb55eef7b28ccaf4a7f6ea9318c64a6c
JunGwon <j464kim@uwaterloo.ca>
2017-05-12 08:58:28 -0400

    resolve conflict

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,6 @@ Route::group(['middleware' => ['auth:api']], function () {
 	Route::post('logout', 'Auth\LoginController@logout')->middleware('auth:api');
 });
 
-
-<<<<<<< HEAD
 // This route group applies the "web" middleware group to every route
 // it contains. The "web" middleware group is defined in your HTTP
 // kernel and includes session state, CSRF protection, and more.
@@ -27,12 +25,9 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::resource('dishes', 'DishController');
     Route::resource('kitchens', 'KitchenController');
 
-=======
-
 // TODO Change this to proper middleware group later on
 Route::resource('dishes', 'DishController');
 
 
 Route::post('login', 'Auth\LoginController@login');
 Route::post('refresh-token', 'Auth\LoginController@refreshToken');
->>>>>>> master
