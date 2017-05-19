ec213f080a4871fef06d9a48d947757b13f47b33
JunGwon <j464kim@uwaterloo.ca>
2017-05-19 02:42:08 -0400

    Figured out Object cannot be passed API as a request , Implemented workaround

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -29,9 +29,11 @@ class MediaController extends Controller
 	 */
 	public function store(Request $request)
 	{
-		$medias = [];
-		$email = $request->input('email');
-		Log::info('email is: ' . $email);
+		$mediable_id = $request->input('mediable_id');
+		$mediable_type = $request->input('mediable_type');
+		Log::info($mediable_id);
+		Log::info($mediable_type);
+
 		$files = $request->file('file');
 		foreach ($files as $file) {
 			if (!$request->hasFile('file')) {
@@ -44,6 +46,7 @@ class MediaController extends Controller
 
 			$fileName = 'yechef_' . uniqid() . '.' . $file->getClientOriginalExtension();
 			$s3 = Storage::disk('s3');
+			// please leave it commented as it costs money to upload file to S3
 //			$s3->put($fileName, file_get_contents($file), 'public');
 			$mimeType = $file->getClientMimeType();
 			Log::info('mimetype is: ' . $mimeType);
@@ -55,18 +58,14 @@ class MediaController extends Controller
 			$this->validateInput($request);
 			Log::info('validated');
 
-			// get created mediable data
-			$kitchen = Kitchen::where('email', $email)->first();
-			Log::info($kitchen->id);
-
 			// Save to local db
 			$media = Media::create([
 				'slug'          => snake_case($fileName),
 				'url'           => env('AWS_URL') . $fileName,
 				// not sure if there is an exact method to determine image or video
 				'type'          => 'image',
-				'mediable_id'   => $kitchen->id,
-				'mediable_type' => get_class($kitchen),
+				'mediable_id'   => $mediable_id,
+				'mediable_type' => $mediable_type
 			]);
 
 			Log::info('media created');
