9d628a70f8844053e09eb8522eea1afe8a5ca6ee
vagrant <vagrant@homestead>
2017-05-26 03:24:16 +0000

    Implement user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,72 +2,106 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Exceptions\YechefException;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Register Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users as well as their
+	| validation and creation. By default this controller uses a trait to
+	| provide this functionality without requiring any additional code.
+	|
+	*/
 
-    use RegistersUsers;
+	use RegistersUsers;
 
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+	// DI parameters
+	private $loginCtrl;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
+	/**
+	 * Where to redirect users after registration.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = '/home';
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct(Application $app, LoginController $loginController)
+	{
+		$this->loginCtrl = $loginController;
+		$this->middleware('guest');
+	}
+
+	public function register(Request $request)
+	{
+		$validator = $this->validator($request->all());
+
+		if ($validator->fails()) {
+			throw new YechefException(10505, $validator->getMessageBag());
+		}
+
+		$first_name = $request->input('first_name');
+		$last_name = $request->input('last_name');
+		$email = $request->input('email');
+		$password = $request->input('password');
+		$phone = $request->input('phone');
+
+		$this->create([
+			'email'      => $email,
+			'password'   => $password,
+			'first_name' => $first_name,
+			'last_name'  => $last_name,
+			'phone'      => $phone,
+		]);
+
+		return $this->loginCtrl->login($request);
+	}
+
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function validator(array $data)
+	{
+		return Validator::make($data, [
 			'first_name' => 'required|max:255',
-			'last_name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
-        ]);
-    }
+			'last_name'  => 'required|max:255',
+			'email'      => 'required|email|max:255|unique:users',
+			'password'   => 'required|min:6|confirmed',
+			'phone'      => 'phone',
+		]);
+	}
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array $data
+	 * @return User
+	 */
+	protected function create(array $data)
+	{
+		return User::create([
 			'first_name' => $data['first_name'],
-			'last_name' => $data['last_name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
+			'last_name'  => $data['last_name'],
+			'phone'      => $data['phone'],
+			'email'      => $data['email'],
+			'password'   => bcrypt($data['password']),
+		]);
+	}
 }
