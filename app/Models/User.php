759f30cce410fcbb98315480ed8236ebeef344fa
JunGwon <j464kim@uwaterloo.ca>
2017-06-12 21:09:43 -0400

    use just created trait in model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Auth\Passwords\CanResetPassword;
+use App\Traits\CanResetPassword;
 
 class User extends Authenticatable
 {
